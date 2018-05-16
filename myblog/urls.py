3a16ef075d62ee5ec436aa30ab0b10261984e198
fengrui.yang <fengrui.yang@11bee.com>
2018-05-16 10:46:06 +0800

    创建模型初步启动

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,6 @@ from . import views;
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^detail/(?P<pk>[0-9])/$', views.detail, name='detail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Category,Post,Tag
 from django.views.generic.list import ListView
 from django.http import HttpResponse
@@ -6,4 +6,10 @@ import markdown2
 # Create your views here.
 
 def index (request):
-    return HttpResponse("welcome")
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'myblog/index.html', context={'post_list': post_list})
+
+def detail(request,pk):
+    post_detail = get_object_or_404(Post,pk=pk)
+    return render(request,'myblog/detail.html' ,context={"postDetail":post_detail})
+
